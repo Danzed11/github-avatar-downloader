0140c086667165308f87ec3d758de1534433aef0
Dan <dan.zajac@mac.com>
2019-05-07 04:24:53 +0000

    added comment lines

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,9 @@
 var request = require('request');
 var secret = require('./secret.js')
 var fs = require('fs')
-<<<<<<< HEAD
-var argv1 = process.argv[2];
-var argv2 = process.argv[3];
-  if(!argv1) {
-    console.log("No input received");
-    process.exit();
-  }
-=======
->>>>>>> 345f94b42f8dfa5d48048f52f9f88e4337d4a0a5
 console.log('Welcome to Github Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) { //Request for JSON, returning array of contribs
    var options = {
      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
      headers: {
@@ -28,21 +19,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
        //   console.log(repo.avatar_url)
          downloadImageByURL(repo.avatar_url, "avatars/" + repo.login + ".jpg")
      });
-     cb(err, body);
+     cb(err, body); //Anon callback function
      });
    };
 
-<<<<<<< HEAD
-   getRepoContributors(argv1, argv2, function(err, result) {
-=======
    getRepoContributors("jquery", "jquery", function(err, result) {
->>>>>>> 345f94b42f8dfa5d48048f52f9f88e4337d4a0a5
         console.log("Errors:", err);
         console.log("Result:", result);
 
      });
 
-   function downloadImageByURL(url, filePath) {
+   function downloadImageByURL(url, filePath) { //Fetches desired avatar_url
    request.get(url)
    .on('error', function (err) {
      throw err;
@@ -52,8 +39,4 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
    })
    .pipe(fs.createWriteStream(filePath));
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 345f94b42f8dfa5d48048f52f9f88e4337d4a0a5
