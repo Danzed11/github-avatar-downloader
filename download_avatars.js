01b0470899c302a7ddc16bf7374a823fa0c7930d
Dan <dan.zajac@mac.com>
2019-05-07 02:03:51 +0000

    Received files for avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-
+var secret = require('./secret.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -7,30 +7,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'request'
+      'Authorization': secret.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
+    var repos = JSON.parse(body);
+    repos.forEach(function(repo) {
+      console.log(repo.avatar_url);
+    })
     cb(err, body);
   });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
 });
-
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options;
-//   var url = request.get('https://api.github.com/repos/jquery/jquery/contributors')
-//   .on('error', function (err) {
-//     throw err;
-//   })
-//   .on('response', function (response) {
-//     console.log('avatar URL: ', body.avatar_url)
-//   })
-//   .on('data', function (data) {
-
-
-//   })
-// }
